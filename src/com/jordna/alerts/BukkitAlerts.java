6f6d7b84d1c1ee9f257b2be047f9df2fa7a5af2f
Jordan <jordan.dalby@yorksj.ac.uk>
2022-03-11 23:29:00 +0000

    Initial commit

---
+++ src/com/jordna/alerts/BukkitAlerts.java
@@ -0,0 +1,21 @@
+package com.jordna.alerts;
+
+public class BukkitAlerts 
+{
+	
+	/*
+	 * Steps for successful fetch and decode of alerts
+	 * 1: Load various app settings, paths, tray icon
+	 * 2: Login to the website
+	 * 3: Keep checking on a delay, the number of alerts reported by bukkit.org/forums/.json
+	 * 4: Use JSON library to decode value
+	 * 5: Ping when an alert is available
+	 * 6: Win
+	 */
+	
+	public static void main(String[] strings)
+	{
+		
+	}
+	
+}
