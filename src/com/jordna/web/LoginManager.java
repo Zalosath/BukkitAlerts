71a6d520d538071645b2b819399790cfb0976533
Jordan <jordan.dalby@yorksj.ac.uk>
2022-03-12 19:44:29 +0000

    Added lots of stuff

--- src/com/jordna/web/LoginManager.java
+++ src/com/jordna/web/LoginManager.java
@@ -17,200 +17,199 @@ import javax.net.ssl.HttpsURLConnection;
 import com.jordna.error.ErrorManager;
 import com.jordna.settings.UserSettings;
 
-public class LoginManager 
+public class LoginManager
 {
 
-	// Manages the whole logging in stuff
-	private ErrorManager errorManager;
-	private UserSettings settings;
-	
-	// Variables
-	private CookieManager cookieManager;
-	private String request = "";
-	private URL url;
-	private HttpsURLConnection conn = null;
+    // Manages the whole logging in stuff
+    private ErrorManager errorManager;
+    private UserSettings settings;
 
-	private boolean loggedIn;
-	
-	public LoginManager(ErrorManager _errorManager, UserSettings _settings)
+    // Variables
+    private CookieManager cookieManager;
+    private String request = "";
+    private URL url;
+    private HttpsURLConnection conn = null;
+
+    private boolean loggedIn;
+
+    public LoginManager(ErrorManager _errorManager, UserSettings _settings)
+    {
+	errorManager = _errorManager;
+	settings = _settings;
+
+	login();
+    }
+
+    private void login()
+    {
+	setupHttpsUrlConnection();
+	setupCookieManager();
+	setupHttpsUrlConnection();
+
+	setupURL("https://bukkit.org");
+	connectURLGet();
+
+	setupURL("https://bukkit.org/login/login");
+	connectURLPost();
+
+    }
+
+    private void setupHttpsUrlConnection()
+    {
+	HttpsURLConnection.setFollowRedirects(true);
+    }
+
+    private void setupCookieManager()
+    {
+	cookieManager = new CookieManager();
+	cookieManager.setCookiePolicy(CookiePolicy.ACCEPT_ALL);
+	CookieHandler.setDefault(cookieManager);
+    }
+
+    private void setupURL(String urlString)
+    {
+	try
 	{
-		errorManager = _errorManager;
-		settings = _settings;
-		
-		login();
+	    url = new URL(urlString);
 	}
-	
-	private void login()
+	catch (MalformedURLException e)
 	{
-		setupHttpsUrlConnection();
-		setupCookieManager();
-		setupHttpsUrlConnection();
-		
-		setupURL("https://bukkit.org");
-		connectURLGet();
-		
-		setupURL("https://bukkit.org/login/login");
-		connectURLPost();
-		
+	    errorManager.setError("Couldn't read URL " + url);
+	    return;
 	}
-	
-	private void setupHttpsUrlConnection()
+    }
+
+    private void connectURLGet()
+    {
+	try
 	{
-		HttpsURLConnection.setFollowRedirects(true);
+	    conn = (HttpsURLConnection) url.openConnection();
 	}
-	
-	private void setupCookieManager()
+	catch (IOException e)
 	{
-	    cookieManager = new CookieManager();
-	    cookieManager.setCookiePolicy(CookiePolicy.ACCEPT_ALL);
-	    CookieHandler.setDefault(cookieManager);
+	    errorManager.setError("IOException on conn");
+	    return;
 	}
-	
-	private void setupURL(String urlString)
+
+	conn.setUseCaches(false);
+
+	try
 	{
-		try
-		{
-			url = new URL(urlString);
-		}
-		catch (MalformedURLException e)
-		{
-			errorManager.setError("Couldn't read URL " + url);
-			return;
-		}
+	    conn.setRequestMethod("GET");
 	}
-	
-	private void connectURLGet()
+	catch (ProtocolException e)
 	{
-		try 
-		{
-			conn = (HttpsURLConnection) url.openConnection();
-		} 
-		catch (IOException e) 
-		{
-			errorManager.setError("IOException on conn");
-			return;
-		}
-		
-	    conn.setUseCaches(false);
-	    
-		try 
-		{
-			conn.setRequestMethod("GET");
-		} 
-		catch (ProtocolException e) 
-		{
-			errorManager.setError("ProtocolException thrown when setting request method");
-			return;
-		}
-	    
-		// Other connection properties
-	    conn.setRequestProperty("Host", "bukkit.org");
-	    conn.setRequestProperty("User-Agent", "Mozilla/5.0");
-	    conn.setRequestProperty("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8");
-	    conn.setRequestProperty("Accept-Language", "en-US,en;q=0.5");
-	    conn.setRequestProperty("Connection", "keep-alive");
-	    conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
-	    conn.setRequestProperty("Content-Length", Integer.toString(request.length()));
-	    
-	    conn.setDoInput(true);
-	    
-	    InputStream inStream;
-		try 
-		{
-			inStream = conn.getInputStream();
-		    InputStreamReader streamReader = new InputStreamReader(inStream);
-		    
-		    BufferedReader in = new BufferedReader(streamReader);
-		    StringBuffer response = new StringBuffer();
-		    
-		    String inputLine;
-			while ((inputLine = in.readLine()) != null)
-				response.append(inputLine);
-		} 
-		catch (IOException e) 
-		{
-			errorManager.setError("IOException with GET request");
-			return;
-		}
-	    
+	    errorManager.setError("ProtocolException thrown when setting request method");
+	    return;
+	}
+
+	// Other connection properties
+	conn.setRequestProperty("Host", "bukkit.org");
+	conn.setRequestProperty("User-Agent", "Mozilla/5.0");
+	conn.setRequestProperty("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8");
+	conn.setRequestProperty("Accept-Language", "en-US,en;q=0.5");
+	conn.setRequestProperty("Connection", "keep-alive");
+	conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+	conn.setRequestProperty("Content-Length", Integer.toString(request.length()));
+
+	conn.setDoInput(true);
+
+	InputStream inStream;
+	try
+	{
+	    inStream = conn.getInputStream();
+	    InputStreamReader streamReader = new InputStreamReader(inStream);
+
+	    BufferedReader in = new BufferedReader(streamReader);
+	    StringBuffer response = new StringBuffer();
+
+	    String inputLine;
+	    while ((inputLine = in.readLine()) != null)
+		response.append(inputLine);
+	}
+	catch (IOException e)
+	{
+	    errorManager.setError("IOException with GET request");
+	    return;
+	}
+
+	conn.disconnect();
+    }
+
+    private void connectURLPost()
+    {
+	request = "login=" + settings.getUsername() + "&register=0&password=" + settings.getPassword()
+		+ "&cookie_check=1&redirect=%2Faccount%2Falerts&_xfToken=\"";
+
+	try
+	{
+	    conn = (HttpsURLConnection) url.openConnection();
+	}
+	catch (IOException e)
+	{
+	    errorManager.setError("Failed to connect to URL");
+	    return;
+	}
+
+	conn.setUseCaches(false);
+
+	try
+	{
+	    conn.setRequestMethod("POST");
+	}
+	catch (ProtocolException e)
+	{
+	    errorManager.setError("ProtocolException thrown when setting request method");
+	    return;
+	}
+
+	conn.setRequestProperty("Host", "bukkit.org");
+	conn.setRequestProperty("User-Agent", "Mozilla/5.0");
+	conn.setRequestProperty("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8");
+	conn.setRequestProperty("Accept-Language", "en-US,en;q=0.5");
+	conn.setRequestProperty("Connection", "keep-alive");
+	conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+	conn.setRequestProperty("Content-Length", Integer.toString(request.length()));
+	conn.setDoOutput(true);
+	conn.setDoInput(true);
+
+	try
+	{
+	    DataOutputStream wr = new DataOutputStream(conn.getOutputStream());
+
+	    wr.writeBytes(request);
+	    wr.flush();
+	    wr.close();
+
+	    @SuppressWarnings("unused")
+	    String cookies = "";
+	    if (conn.getResponseCode() == 303) cookies = conn.getHeaderField("Set-Cookie");
+
+	    InputStream inputStream1 = conn.getInputStream();
+	    InputStreamReader inputStreamReader1 = new InputStreamReader(inputStream1);
+
+	    BufferedReader bufferedReader1 = new BufferedReader(inputStreamReader1);
+	    StringBuffer stringBuffer1 = new StringBuffer();
+
+	    String str1;
+	    while ((str1 = bufferedReader1.readLine()) != null)
+		stringBuffer1.append(str1);
+
 	    conn.disconnect();
-	}
-	
-	private void connectURLPost()
-	{
-		request = "login=" + settings.getUsername() + "&register=0&password=" + settings.getPassword() + "&cookie_check=1&redirect=%2Faccount%2Falerts&_xfToken=\"";
-		
-		try 
-		{
-			conn = (HttpsURLConnection) url.openConnection();
-		} 
-		catch (IOException e) 
-		{
-			errorManager.setError("Failed to connect to URL");
-			return;
-		}
-		
-	    conn.setUseCaches(false);
-	    
-	    try 
-	    {
-			conn.setRequestMethod("POST");
-		} 
-	    catch (ProtocolException e) 
-	    {
-			errorManager.setError("ProtocolException thrown when setting request method");
-	    	return;
-		}
-	    
-	    conn.setRequestProperty("Host", "bukkit.org");
-	    conn.setRequestProperty("User-Agent", "Mozilla/5.0");
-	    conn.setRequestProperty("Accept", 
-	        "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8");
-	    conn.setRequestProperty("Accept-Language", "en-US,en;q=0.5");
-	    conn.setRequestProperty("Connection", "keep-alive");
-	    conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
-	    conn.setRequestProperty("Content-Length", Integer.toString(request.length()));
-	    conn.setDoOutput(true);
-	    conn.setDoInput(true);
-		
-		try 
-		{
-			DataOutputStream wr = new DataOutputStream(conn.getOutputStream());
+	    bufferedReader1.close();
 
-		    wr.writeBytes(request);
-		    wr.flush();
-		    wr.close();
-		    
-		    @SuppressWarnings("unused")
-			String cookies = "";
-		    if (conn.getResponseCode() == 303)
-		    	cookies = conn.getHeaderField("Set-Cookie"); 
-		    
-		    InputStream inputStream1 = conn.getInputStream();
-		    InputStreamReader inputStreamReader1 = new InputStreamReader(inputStream1);
-		    
-		    BufferedReader bufferedReader1 = new BufferedReader(inputStreamReader1);
-		    StringBuffer stringBuffer1 = new StringBuffer();
-		    
-		    String str1;
-		    while ((str1 = bufferedReader1.readLine()) != null)
-		      stringBuffer1.append(str1); 
-		    
-		    conn.disconnect();
-		    bufferedReader1.close();
-		    
-		    loggedIn = true;
-		} 
-		catch (IOException e)
-		{
-			errorManager.setError("Failed to read data from POST connection");
-			return;
-		}
+	    loggedIn = true;
 	}
-	
-	public boolean hasLoggedIn()
+	catch (IOException e)
 	{
-		return loggedIn;
+	    errorManager.setError("Failed to read data from POST connection");
+	    return;
 	}
-	
+    }
+
+    public boolean hasLoggedIn()
+    {
+	return loggedIn;
+    }
+
 }
